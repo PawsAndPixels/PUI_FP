7450abe4144929c2c6d9ba4802eaeb02e98e5b75
PawsAndPixels <143561108+PawsAndPixels@users.noreply.github.com>
2023-12-11 16:32:51 -0500

    changes

--- JS/EyeBall.js
+++ JS/EyeBall.js
@@ -1,8 +1,8 @@
 function eyeBalls(event) {
   let balls = document.getElementsByClassName("ball");
 
-  let x = (event.clientX * 100) / window.innerWidth + "%"; //Calculates the horizontal position of the mouse as a percentage of the window's width.
-  let y = (event.clientY * 100) / window.innerHeight + "%"; //Calculates the vertical position of the mouse cursor as a percentage of the window's height.
+  let x = (event.clientX * 100) / window.innerWidth + "%"; //calculates the horizontal position of the mouse as a percentage of the window's width.
+  let y = (event.clientY * 100) / window.innerHeight + "%"; //calculates the vertical position of the mouse cursor as a percentage of the window's height.
 
   //event.clientX => gets horizontal coordinate of the mouse
   //event.clientY => gets vertical coordinate of the mouse
